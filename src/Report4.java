1d7dde7790b3b1b9fb6f11651d6c3b81fb9f2dd7
Sakugawa Masataka <e185702@ie.u-ryukyu.ac.jp>
2018-11-20 20:57:41 +0900

    3rd commit

--- src/Report4.java
+++ src/Report4.java
@@ -1,16 +1,13 @@
 public class Report4 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
 
-        try {
-            System.out.println(str.length());
-        } catch (Exception e) {
-            System.out.println("ぬるぽがでたお^p^");
-            System.out.println(e.getMessage());
-        }
+                //try {
+                    //System.out.println(str.length());
+                //} catch (Exception e) {
+                    //System.out.println("ぬるぽ");
+                    //System.out.println(e.getMessage());
+                //}
     }
 }
-
-
-
