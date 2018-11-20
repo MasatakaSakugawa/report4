00646704b540b6174b93ae247635359101a5e149
Sakugawa Masataka <e185702@ie.u-ryukyu.ac.jp>
2018-11-20 20:05:34 +0900

    2nd commit

--- src/Report4.java
+++ src/Report4.java
@@ -1,6 +1,16 @@
 public class Report4 {
-    public static void main (String[]args){
+    public static void main(String[] args) {
         String str = null;
         System.out.println(str.length());
+
+        try {
+            System.out.println(str.length());
+        } catch (Exception e) {
+            System.out.println("ぬるぽがでたお^p^");
+            System.out.println(e.getMessage());
+        }
     }
 }
+
+
+
